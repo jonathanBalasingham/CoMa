f7a928f306e4b71c9db3c1cee337e11602c37134
jonathanBalasingham <jonbalasin@gmail.com>
2020-12-19 22:45:47 +0000

    added getters and setters for Matrix class and filled out MatOps some more

--- Matrix.h
+++ Matrix.h
@@ -10,24 +10,26 @@
 
 class Matrix {
 public:
+    Matrix(std::tuple<int,int> sh){ i=std::get<0>(sh); j=std::get<1>(sh); data=new float[i*j]; }
+    Matrix(int a, int b){ i = a; j = b; data = new float[i*j]; }
+    ~Matrix(){ delete [] this->data; }
+    [[nodiscard]] std::tuple<int,int> shape() const { return std::make_tuple(this->i, this->j); }
+    [[nodiscard]] int inline entries() const{ return this->i * this->j; }
+    int get_i() const { return i; }
+    int get_j() const { return j; }
+    float get(int x, int y) const { return data[x*i + y]; }
+    float set(int x, int y, float z){ return data[x*i + y] = z; }
+    static Matrix* zeros(int i, int j){ return init_all(i,j, 0.); }
+    static Matrix* ones(int i, int j){ return init_all(i,j, 1.); }
+    static Matrix* zeros(std::tuple<int,int> sh){ return zeros(std::get<0>(sh), std::get<1>(sh)); }
+    static Matrix* ones(std::tuple<int,int> sh){ return zeros(std::get<0>(sh), std::get<1>(sh)); }
+
     float operator[](int i) const {
         return this->data[i];
     }
-    Matrix(){}
-    Matrix(int i, int j){
-        this->i = i;
-        this->j = j;
-        this->data = new float[i*j];
-    }
-    ~Matrix(){ delete [] this->data; }
-    [[nodiscard]] std::tuple<int,int> shape() const { return std::make_tuple(this->i, this->j); }
-    int inline entries() const{ return this->i * this->j; }
-
-    static Matrix* zeros(int i, int j){ return init_all(i,j, 0.); }
-    static Matrix* ones(int i, int j){ return init_all(i,j, 1.); }
 
     Matrix* copy(){
-        auto m = new Matrix();
+        auto m = new Matrix(this->shape());
         for(int ind = 0; i < entries(); ind++)
             m->data[ind] = data[ind];
         return m;
@@ -59,17 +61,25 @@ public:
     }
 
     Matrix* operator-(const Matrix &B){
-        if(B.shape() != this->shape()) throw std::invalid_argument("Matrices must have the same shape.");
+        if(B.shape() != shape()) throw std::invalid_argument("Matrices must have the same shape.");
         auto m = new Matrix(i,j);
         for (int a = 0; a < entries(); a++) { m->data[a] = data[a] - B[a]; }
         return m;
     }
+
+    Matrix* reshape(int x, int y){
+        auto m = copy();
+        m->i = x;
+        m->j = y;
+        return m;
+    }
+
 private:
     float* data;
     int i,j = 0;
-    static Matrix* init_all(int i, int j, float e){
-        auto m = new Matrix(i,j);
-        for(int i = 0; i < m->entries(); i++) m->data[i] = e;
+    static Matrix* init_all(int a, int b, float e){
+        auto m = new Matrix(a,b);
+        for(a = 0; a < m->entries(); a++) m->data[a] = e;
         return m;
     }
 };
--- MatrixOperations.h
+++ MatrixOperations.h
@@ -6,27 +6,49 @@
 #define COMA_MATRIXOPERATIONS_H
 
 #include "Matrix.h"
+#include <math.h>
 
 namespace CoMa {
 
-    Matrix lower_triangle(float ** A){
+    bool is_square(const Matrix &A){
+        return std::get<0>(A.shape()) == std::get<1>(A.shape());
+    }
 
+    Matrix* lower_triangle(const Matrix &A){
+        if (!is_square(A))
+            throw std::invalid_argument("Matrix must be square");
+        auto m = Matrix::zeros(A.shape());
+        int i = std::get<0>(A.shape());
+        int j = std::get<1>(A.shape());
+        for (int a = 0; a < A.entries(); a++){
+            if (a % i <= floor(j / 2) + 1 )
+        }
+        return m;
     }
 
     Matrix upper_triangle(Matrix A){
-
+        if (!is_square(A))
+            throw std::invalid_argument("Matrix must be square");
     }
 
     Matrix strict_lower_triangle(Matrix A){
-
+        if (!is_square(A))
+            throw std::invalid_argument("Matrix must be square");
     }
 
     Matrix strict_upper_triangle(Matrix A){
-
+        if (!is_square(A))
+            throw std::invalid_argument("Matrix must be square");
     }
 
-    Matrix transpose(Matrix A){
-
+    Matrix* transpose(const Matrix &A){
+        auto m = new Matrix(A.get_j(), A.get_i());
+        for (int a = 0; a < A.get_j(); a++){
+            for (int b = 0; b < A.get_i(); b++){
+                m->set(a, b, A.get(b,a));
+            }
+        }
+        return m;
     }
 
 
