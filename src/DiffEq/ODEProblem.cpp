4df67af0e338f6858e47d26be9da5b8670e80e84
jonathanBalasingham <jonbalasin@gmail.com>
2021-02-17 22:38:40 +0000

    Add ODE scaffolding

---
+++ src/DiffEq/ODEProblem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jon on 2/17/21.
+//
+
+#include "ODEProblem.h"
---
+++ src/DiffEq/ODEProblem.h
@@ -0,0 +1,14 @@
+//
+// Created by jon on 2/17/21.
+//
+
+#ifndef COMA_ODEPROBLEM_H
+#define COMA_ODEPROBLEM_H
+
+
+class ODEProblem {
+
+};
+
+
+#endif //COMA_ODEPROBLEM_H
